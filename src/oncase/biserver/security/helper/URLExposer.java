adfb2579297456e99093b094524819b49a89fe36
Marcello Pontes <balaum@gmail.com>
2017-12-22 19:07:20 +0100

    DEV commit - it works
    This can't be used like this - I need to clean up the code and leave
    only what's necessary

--- src/oncase/biserver/security/helper/URLExposer.java
+++ src/oncase/biserver/security/helper/URLExposer.java
@@ -1,144 +1,88 @@
 package oncase.biserver.security.helper;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import org.pentaho.platform.api.engine.IPentahoObjectReference;
+import javax.servlet.Filter;
+
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.core.system.objfac.spring.PublishedBeanRegistry;
 import org.pentaho.platform.web.http.context.PentahoSolutionSpringApplicationContext;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.ConstructorArgumentValues;
-import org.springframework.beans.factory.config.ConstructorArgumentValues.ValueHolder;
-import org.springframework.beans.factory.config.ListFactoryBean;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 public class URLExposer implements BeanFactoryPostProcessor {
 
-	private FilterInvocationSecurityMetadataSource securityMetadataSource;
+	private String filterChainUrlPattern;
+	private FilterSecurityInterceptor filterInterceptor;
+	
+	private PentahoSolutionSpringApplicationContext appContext;
+	private FilterChainProxy fcpObject;
 	
 
 	@Override
 	public void postProcessBeanFactory(
 			ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		
 		System.out.println("\n\n\n**URLExposer.postProcessBeanFactory(...)**");
-		
 
-		PentahoSolutionSpringApplicationContext appContext = getSpringContext();
-		GenericBeanDefinition fcp = (GenericBeanDefinition) appContext.getBeanFactory().getBeanDefinition("filterChainProxy");
-			
-		ConstructorArgumentValues argValues = fcp.getConstructorArgumentValues();
-		List<ValueHolder> indexedArgValues = argValues.getGenericArgumentValues();
-		Iterator it = indexedArgValues.iterator();
-		ValueHolder constructorValueHolder = new ValueHolder(new Object());
-		
-		//TODO: find a better way yo assign the value holder
-		while(it.hasNext()){
-			constructorValueHolder = (ValueHolder) it.next();
-		}
-		BeanDefinitionHolder constructorBeanDefHolder = (BeanDefinitionHolder) constructorValueHolder.getValue();
-		GenericBeanDefinition listBeanDefinition = (GenericBeanDefinition) constructorBeanDefHolder.getBeanDefinition();
+		initSpringContext();
+		initFcpObject();
 
-		
 		
 		/* Quest for the list */
 		List<SecurityFilterChain> lsf = new ArrayList<SecurityFilterChain>();
 		
 		System.out.println("..................");
 				
-		FilterChainProxy fcpObject = (FilterChainProxy) appContext.getBean("filterChainProxy");
+		
 		lsf = new ArrayList<SecurityFilterChain>( fcpObject.getFilterChains() );
-		lsf.add(0, getCustomFilterChain());
-		argValues.clear();
-		argValues.addGenericArgumentValue(getNewProxyList(), "java.util.List");
-		fcp.setConstructorArgumentValues(argValues);
+
+		System.out.println(".................. SIZE OF THE LIST");
+		System.out.println(lsf.size());
+		
+		lsf.add(lsf.size()-1, getCustomFilterChain());
+		
+		System.out.println(".................. NEW SIZE OF THE LIST");
+		System.out.println(lsf.size());
+
 		FilterChainProxy proxy2 = new FilterChainProxy(lsf);
 		/*/Quest for the list */
 		
-		
 		appContext.getBeanFactory().registerSingleton(proxy2.getClass().getCanonicalName(), proxy2);
 		
-		appContext.getBeanFactory().getSingleton(proxy2.getClass().getCanonicalName()).getClass().getCanonicalName();
-		
-		
-		//problema -> pegar a lista
-//		argValues.clear();
-//		argValues.addGenericArgumentValue(getNewProxyList(), "java.util.List");
-//		fcp.setConstructorArgumentValues(argValues);
-
-		
-
 	}
 	
-	private PentahoSolutionSpringApplicationContext getSpringContext(){
+	private void initFcpObject() {
+		fcpObject = (FilterChainProxy) appContext.getBean("filterChainProxy");
+	}
+
+	private void initSpringContext(){
 		Set<ListableBeanFactory> factories = PublishedBeanRegistry.getRegisteredFactories();
 		ListableBeanFactory[] factoriesArr = factories.toArray(new ListableBeanFactory[factories.size()]);
 		
 		for(int x = 0 ; x < factoriesArr.length ; x++ ){
 			
 			if(factoriesArr[x] instanceof PentahoSolutionSpringApplicationContext){
-				PentahoSolutionSpringApplicationContext appContext = (PentahoSolutionSpringApplicationContext) factoriesArr[x];
-				return appContext;
+				PentahoSolutionSpringApplicationContext appCon = (PentahoSolutionSpringApplicationContext) factoriesArr[x];
+				this.appContext = appCon;
+				break;
 			}
 		}
-		return null;
 	}
 
-	private FilterChainProxy getNewProxy(){
-		FilterChainProxy proxy = getProxy();
-		List<SecurityFilterChain> list = proxy.getFilterChains();
-		
-		ArrayList<SecurityFilterChain> l2 = new ArrayList<SecurityFilterChain>();
-		l2.addAll(list);
-		DefaultSecurityFilterChain fc1 = 
-					new DefaultSecurityFilterChain(
-							new AntPathRequestMatcher("/content/integrator/**"), getCustomInterceptor());
-		
-		l2.add(0, fc1);
-		
-		FilterChainProxy proxy2 = new FilterChainProxy(l2);
-		return proxy2;
-		
-	}
-	
-	private List<SecurityFilterChain> getNewProxyList(){
-		List<SecurityFilterChain> lsf = new ArrayList<SecurityFilterChain>();
-		
-		Set<ListableBeanFactory> factories = PublishedBeanRegistry.getRegisteredFactories();
-		ListableBeanFactory[] factoriesArr = factories.toArray(new ListableBeanFactory[factories.size()]);
-		
-		System.out.println("..................");
-		for(int x = 0 ; x < factoriesArr.length ; x++ ){
-			
-			if(factoriesArr[x] instanceof PentahoSolutionSpringApplicationContext){
-				PentahoSolutionSpringApplicationContext appContext = (PentahoSolutionSpringApplicationContext) factoriesArr[x];
-				FilterChainProxy fcp = (FilterChainProxy) appContext.getBean("filterChainProxy");
-				lsf = new ArrayList<SecurityFilterChain>( fcp.getFilterChains() );
-				lsf.add(getCustomFilterChain());
-			}
-		}
-		return lsf;
-	}
 
 	
 	/**
@@ -155,24 +99,42 @@ public class URLExposer implements BeanFactoryPostProcessor {
 	private FilterSecurityInterceptor getCustomInterceptor() {
 		ProviderManager authenticationManager = PentahoSystem.get(ProviderManager.class,
 				"authenticationManager", PentahoSessionHolder.getSession());
-		FilterSecurityInterceptor interceptor = new FilterSecurityInterceptor();
-		interceptor.setSecurityMetadataSource(securityMetadataSource);
-		interceptor.setAuthenticationManager((AuthenticationManager) authenticationManager);
-		return interceptor;
+		AffirmativeBased accessDecisionManager = PentahoSystem.get(AffirmativeBased.class,
+				"accessDecisionManager", PentahoSessionHolder.getSession());
+
+		filterInterceptor.setAuthenticationManager( authenticationManager );
+		filterInterceptor.setAccessDecisionManager( accessDecisionManager ); 
+		return filterInterceptor;
 	}
 	
 	private DefaultSecurityFilterChain getCustomFilterChain(){
+		// Get the last sec:filter-chain /**
+		SecurityFilterChain sfc = fcpObject.getFilterChains().get(fcpObject.getFilterChains().size()-1);
+		
+		List<Filter> filters = sfc.getFilters();
+		filters.set(filters.size()-1, getCustomInterceptor());
 		DefaultSecurityFilterChain fc1 = 
 				new DefaultSecurityFilterChain(
-						new AntPathRequestMatcher("/content/integrator/**"), getCustomInterceptor());
+						new AntPathRequestMatcher(filterChainUrlPattern), 
+						filters );
 		return fc1;
 	}
 
-	public FilterInvocationSecurityMetadataSource getSecurityMetadataSource() {
-		return securityMetadataSource;
+	public String getFilterChainUrlPattern() {
+		return filterChainUrlPattern;
 	}
 
-	public void setSecurityMetadataSource(FilterInvocationSecurityMetadataSource securityMetadataSource) {
-		this.securityMetadataSource = securityMetadataSource;
+	public FilterSecurityInterceptor getFilterInterceptor() {
+		return filterInterceptor;
 	}
+
+	public void setFilterChainUrlPattern(String filterChainUrlPattern) {
+		this.filterChainUrlPattern = filterChainUrlPattern;
+	}
+
+	public void setFilterInterceptor(FilterSecurityInterceptor filterInterceptor) {
+		this.filterInterceptor = filterInterceptor;
+	}
+
+
 }
